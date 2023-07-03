d3a00aa72678b37ebae5faffcda167b81b174f9d
Your Name <you@example.com>
2023-07-03 11:03:00 +0200

    public

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
-include "htmls/header.html";
-include "htmls/menu.html";
+include "../htmls/header.html";
+include "../htmls/menu.html";
 ?>
 <h1>Willkommen auf meiner Statistikseite zu Marvel Champions</h1>
-<?php include "htmls/footer.html"
+<?php include ".../htmls/footer.html"
     ?>
 <script src="" async defer></script>
